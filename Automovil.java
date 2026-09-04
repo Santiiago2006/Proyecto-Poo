c09bc89755f5521c7ea1c49909689318864a1b74
Angel <aarcila@uniempresarial.edu.co>
2026-09-03 22:04:14 -0500

    se agregaron otro metodo los gett y sett y su overrides

--- Automovil.java
+++ Automovil.java
@@ -13,4 +13,52 @@ public class Automovil extends Vehiculo {
         this.tieneAireAcondicionado = false;
         this.cilindraje = 0.0;
     }
+    public Automovil(String placa, int año, double precio, boolean disponible,
+                      int numPuertas, String tipoTransmision,
+                      boolean tieneAireAcondicionado, double cilindraje) {
+        super(placa, año, precio, disponible);
+        this.numPuertas = numPuertas;
+        this.tipoTransmision = tipoTransmision;
+        this.tieneAireAcondicionado = tieneAireAcondicionado;
+        this.cilindraje = cilindraje;
+    }
+
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public String getTipoTransmision() {
+        return tipoTransmision;
+    }
+
+    public void setTipoTransmision(String tipoTransmision) {
+        this.tipoTransmision = tipoTransmision;
+    }
+
+    public boolean isTieneAireAcondicionado() {
+        return tieneAireAcondicionado;
+    }
+
+    public void setTieneAireAcondicionado(boolean tieneAireAcondicionado) {
+        this.tieneAireAcondicionado = tieneAireAcondicionado;
+    }
+
+    public double getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" +
+                "placa='" + placa + ", año=" + año +", precio=" + precio +", disponible=" + disponible + ", numPuertas=" + numPuertas +", tipoTransmision='" + tipoTransmision + ", tieneAireAcondicionado=" + tieneAireAcondicionado +", cilindraje=" + cilindraje;
+    }
 }  
