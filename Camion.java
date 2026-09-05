4084d0a088adcabde7f2dd99509f915bfa41c8ed
Angel <aarcila@uniempresarial.edu.co>
2026-09-04 21:22:03 -0500

    se agrego la clase camnion con sus atributos y 2 metodos constructores

---
+++ Camion.java
@@ -0,0 +1,25 @@
+public class Camion extends Vehiculo {
+
+    private double capacidadCarga;
+    private int numEjes;
+    private String tipoCarroceria;
+    private boolean tieneRemolque;
+
+    
+    public Camion(double capacidadCarga, int numEjes) {
+        super("SIN-PLACA", 2024);
+        this.capacidadCarga = capacidadCarga;
+        this.numEjes = numEjes;
+        this.tipoCarroceria = "No definido";
+        this.tieneRemolque = false;
+    }
+
+    public Camion(String placa, int año, double precio,
+                  double capacidadCarga, int numEjes, String tipoCarroceria) {
+        super(placa, año, precio);
+        this.capacidadCarga = capacidadCarga;
+        this.numEjes = numEjes;
+        this.tipoCarroceria = tipoCarroceria;
+        this.tieneRemolque = false;
+    }
+}
