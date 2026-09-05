8e0c26e1a952aaa28e990435c4e56b89939e3404
Angel <aarcila@uniempresarial.edu.co>
2026-09-04 19:59:51 -0500

    se agregaron los demas metodos

--- Motocicleta.java
+++ Motocicleta.java
@@ -13,4 +13,23 @@ public class Motocicleta extends Vehiculo {
         this.autonomiaTanque = 0.0;
     }
     
+    public Motocicleta(String placa, int anio, double precio,
+                        int cilindrajeCC, String tipoMoto, boolean tieneMaletero) {
+        super(placa, anio, precio);
+        this.cilindrajeCC = cilindrajeCC;
+        this.tipoMoto = tipoMoto;
+        this.tieneMaletero = tieneMaletero;
+        this.autonomiaTanque = 0.0;
+    }
+
+    public Motocicleta(String placa, int anio, double precio, boolean disponible,
+                        int cilindrajeCC, String tipoMoto,
+                        boolean tieneMaletero, double autonomiaTanque) {
+        super(placa, anio, precio, disponible);
+        this.cilindrajeCC = cilindrajeCC;
+        this.tipoMoto = tipoMoto;
+        this.tieneMaletero = tieneMaletero;
+        this.autonomiaTanque = autonomiaTanque;
+    }
+
 }
